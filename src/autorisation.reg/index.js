09ecf4b36b2f6c93db956447d1d957124ff05f8e
artegoser <59178854+artegoser@users.noreply.github.com>
2020-11-23 22:11:11 +0300

    autorisation/reg 0.5

--- src/autorisation.reg/index.js
+++ src/autorisation.reg/index.js
@@ -1,17 +1,38 @@
-const dgram = require('dgram');
+const dgram = require("dgram");
+const $ = require("Jquery");
 
-let me = {
-        "type":"autorisation",
-        "message":{"name":"name", "password":"name"}
-        }
+const client = dgram.createSocket("udp4");
 
-const message = Buffer.from(JSON.stringify(me));
-const client = dgram.createSocket('udp4');
+window.onload = function() {
+  let me = {
+    type: "autorisation",
+    message: { name: $("#login").val(), password: $("#password").val() },
+  };
 
-client.send(message, 9191, 'artegoser.tplinkdns.com', (err) => {
-  //client.close();
-});
+  $("#reg").on("click", (evt) => {
+    me.type = "register"
+    $("#text").html("registration") 
+  });
 
-client.on('message', function(message, remote) {
- console.log(message.toString());
-});
+  $("#aut").on("click", (evt) => {
+    me.type = "autorisation"
+    $("#text").html("autorisation")
+  });
+    
+  $("#sig").on("click", (evt) => {
+    me.message.name = $("#login").val()
+    me.message.password = $("#password").val()
+
+    let message = Buffer.from(JSON.stringify(me));
+    client.send(message, 9191, "artegoser.tplinkdns.com", (err) => {
+      //console.log(err)
+    });
+  
+    client.on("message", function (message, remote) {
+      console.log(message.toString());
+      let resp = message.toString()
+    });
+  });
+  
+  
+};
