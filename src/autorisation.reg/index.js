c7397b0ec037ec39e5cd09543af091da81748394
artegoser <59178854+artegoser@users.noreply.github.com>
2020-11-23 17:38:54 +0300

    tests

---
+++ src/autorisation.reg/index.js
@@ -0,0 +1,12 @@
+const dgram = require('dgram');
+
+let me = {
+        "type":"autorisation",
+        "message":{"name":"name", "password":"name"}
+        }
+
+const message = Buffer.from(JSON.stringify(me));
+const client = dgram.createSocket('udp4');
+client.send(message, 9191, 'artegoser.tplinkdns.com', (err) => {
+  client.close();
+});
