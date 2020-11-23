09ed13dfc36e8ca5f42b4532a763ab08dc16449d
artegoser <59178854+artegoser@users.noreply.github.com>
2020-11-23 19:27:46 +0300

    tests2

--- src/autorisation.reg/index.js
+++ src/autorisation.reg/index.js
@@ -7,6 +7,11 @@ let me = {
 
 const message = Buffer.from(JSON.stringify(me));
 const client = dgram.createSocket('udp4');
+
 client.send(message, 9191, 'artegoser.tplinkdns.com', (err) => {
-  client.close();
+  //client.close();
+});
+
+client.on('message', function(message, remote) {
+ console.log(message.toString());
 });
