094b1f343d85b7e779b4c90f9fea46ee6597c787
artegoser <59178854+artegoser@users.noreply.github.com>
2020-11-23 15:46:06 +0300

    format code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
+const { app, BrowserWindow } = require("electron");
 
-  
-const {
-    app,
-    BrowserWindow
-} = require('electron');
+const localStorage = require("./modules/localStorage");
 
-const localStorage = require('./modules/localStorage');
-
-const fs = require('fs');
+const fs = require("fs");
 
 const SIZE = {
-    x: 695,
-    y: 495
-}
+  x: 695,
+  y: 495,
+};
 
-app.on('ready', () => {
-    const win = new BrowserWindow({
-        minWidth: SIZE.x,
-        minHeight: SIZE.y,
-		maxWidth: SIZE.x,
-        maxHeight: SIZE.y,
-        //backgroundColor: '#404040',
-        webPreferences: {
-            devTools: true,
-            nodeIntegration: true
-        }
-    });
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    minWidth: SIZE.x,
+    minHeight: SIZE.y,
+    maxWidth: SIZE.x,
+    maxHeight: SIZE.y,
+    //backgroundColor: '#404040',
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: true,
+    },
+  });
 
-    win.webContents.on('dom-ready', () => {
-        win.webContents.openDevTools();
-    });
-	if(localStorage.getItem('autorisation')){
-		win.loadURL(`file://${__dirname}/mainwindow/index.html`);
-	}else{
-		win.loadURL(`file://${__dirname}/autorisation.reg/index.html`);
-	}
-})
+  win.webContents.on("dom-ready", () => {
+    win.webContents.openDevTools();
+  });
+  if (localStorage.getItem("autorisation")) {
+    win.loadURL(`file://${__dirname}/mainwindow/index.html`);
+  } else {
+    win.loadURL(`file://${__dirname}/autorisation.reg/index.html`);
+  }
+});
