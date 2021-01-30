0537e50dce5d30e81dbaa33357c189918eefd180
artegoser <59178854+artegoser@users.noreply.github.com>
2021-01-30 10:34:04 +0300

    cost + transaction

--- src/mainwindow/cost/index.js
+++ src/mainwindow/cost/index.js
@@ -4,5 +4,22 @@ const client = dgram.createSocket("udp4");
 const localStorage = require("../../modules/localStorage");
 
 window.onload = () => {
-    
-};
+    $("#update").on("click", (evt) => {
+        let me = {
+            type:"cost",
+            message:{amount: parseFloat($("#amount").val()) }
+        }
+        let message = Buffer.from(JSON.stringify(me));
+        client.send(message, 9191, "artegoser.tplinkdns.com", (err) => {
+        if (err) {
+            console.log(err);
+            $("#info").html(err);
+        }
+    });
+    client.on("message", function (message, remote) {
+        let resp = message.toString();
+        resp = resp.replace(new RegExp ("SKL_coin", "g"), "SKL_coin<br>")
+        $("#info").html(resp)
+    });
+    });
+}
--- src/mainwindow/transaction/index.js
+++ src/mainwindow/transaction/index.js
@@ -4,5 +4,36 @@ const client = dgram.createSocket("udp4");
 const localStorage = require("../../modules/localStorage");
 
 window.onload = () => {
-    
+    let me = {
+        type: "autorisation",
+        message: { name: localStorage.getItem("login"), password: localStorage.getItem("password") },
+    };
+    let message = Buffer.from(JSON.stringify(me));
+    client.send(message, 9191, "artegoser.tplinkdns.com", (err) => {
+      if (err) {
+        console.log(err);
+        $("#info").html(err);
+      }
+    });
+    $("#send").on("click", (evt) => {
+        me = {
+        type:"transaction",
+        message:{sender: localStorage.getItem("login"), 
+                 recipient: $("#recipient").val(), 
+                 amount: parseFloat($("#amount").val())}
+        }
+
+        let message = Buffer.from(JSON.stringify(me));
+        client.send(message, 9191, "artegoser.tplinkdns.com", (err) => {
+            if (err) {
+                console.log(err);
+                $("#warn").html(err);
+              }
+        });
+    });
+
+    client.on("message", function (message, remote) {
+        let resp = message.toString();
+        $("#info").html(resp)
+    });
 };
